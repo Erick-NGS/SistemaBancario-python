e481d5f67b99059f8387d509b9f67add920dc0eb
Erick-NGS <engs.3003@gmail.com>
2024-09-16 17:37:48 -0300

    feat: creating main menu template

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-#SAQUE
+menu = """
+
+[d] Depositar
+[s] Sacar
+[e] Extrato
+[q] Sair
+
+=> """
+
+saldo_conta = 0
+limite_valor_saque = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+    opcao = input(menu)
+
+    if opcao.lower() == "d":
+        pass
+
+    elif opcao.lower() == "s":
+        pass
+
+    elif opcao.lower() == "e":
+        pass
+
+    elif opcao.lower() == "q":
+        break
+
+    else:
+        print("Opção inválida! Selecione uma opção válida!")
